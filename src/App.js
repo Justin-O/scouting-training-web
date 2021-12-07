2e024c310e00b80becf77cea9cb8854e9a78b937
Justin-O <youtubejustin@live.nl>
2021-12-07 17:16:01 +0100

    Router and bootstrap added and nav setup for build

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Link,
+  Routes,
+  Route
+} from "react-router-dom";
+import Modules from './components/Modules';
+import Badges from './components/Badges';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.          
-        </p>
-        <p>
-          Deze tekst heb ik via mijn laptop op de site gezet.
-        </p>
-        <p>
-          En dit is weer toegevoegd aan app op mijn PC.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="container-fluid justify-content-start">
+          <a href="/modules" className="navbar-brand">
+            Scouting Academy
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/modules" className="nav-link">
+                Modules
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/badges" className="nav-link">
+                Badges
+              </Link>
+            </li>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container-fluid">
+        <Routes>
+          <Route path="modules" element={<Modules />} />
+          <Route path="badges" element={<Badges />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
