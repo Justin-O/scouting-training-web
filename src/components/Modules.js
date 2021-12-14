270805f7d9a409a823fa03ad9ab2d03b341223af
Justin Oosterop <justinplaystation@live.nl>
2021-12-14 10:36:43 +0100

    Module page front end sprint 2
    Modules sprint 2 version front end

--- src/components/Modules.js
+++ src/components/Modules.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import modulesPlaceholder from '../assets/img/homepage-roadmap.png';
+import modulesPlaceholder from '../assets/img/homepage-roadmap.png'
+import '../assets/css/modules.css'
+import ModuleBadges from './ModuleBadges';
 
 function Modules() {
     return (
-        <div>
-            <h1>Modules</h1>
-            <img src={modulesPlaceholder} alt="Modules placeholder" width="100%"></img>
-        </div>
+        <>
+            <ModuleBadges />
+            <div className='row row-padding'>
+                {/* <h1>Modules</h1> */}
+                {/* <img src={modulesPlaceholder} alt="Modules placeholder" width="80%"></img> */}
+                {/* <div className='col'> */}
+                <div className='module-placeholder'></div>
+                {/* </div> */}
+
+            </div>
+        </>
     )
 }
 
